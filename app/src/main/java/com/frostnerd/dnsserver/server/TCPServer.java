768d3f2f37a0bdceff49028f0e816204d2a6a810
Daniel Wolf <daniel.wolf@frostnerd.com>
2018-01-05 18:22:31 +0100

    TCPServer now extends DNSServer (without logic)

--- app/src/main/java/com/frostnerd/dnsserver/server/TCPServer.java
+++ app/src/main/java/com/frostnerd/dnsserver/server/TCPServer.java
@@ -1,8 +1,26 @@
 package com.frostnerd.dnsserver.server;
 
+import android.content.Context;
+
+import com.frostnerd.dnsserver.database.entities.main.DNSServerSetting;
+
 /**
  * Created by Daniel on 25.10.2017.
  */
 
-public class TCPServer {
+public class TCPServer extends DNSServer{
+
+    public TCPServer(Context context, DNSServerSetting settings) {
+        super(context, settings);
+    }
+
+    @Override
+    protected void stopServer() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
