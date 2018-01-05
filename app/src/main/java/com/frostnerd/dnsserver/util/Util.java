939d29faa8a669756046cf10aeb6c2f1b6a181b3
Daniel Wolf <daniel.wolf@frostnerd.com>
2018-01-05 18:27:05 +0100

    Added method for creating notification channels

--- app/src/main/java/com/frostnerd/dnsserver/util/Util.java
+++ app/src/main/java/com/frostnerd/dnsserver/util/Util.java
@@ -1,14 +1,14 @@
 package com.frostnerd.dnsserver.util;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 
+import com.frostnerd.dnsserver.R;
 import com.frostnerd.dnsserver.database.DatabaseHelper;
-import com.frostnerd.dnsserver.database.ServerDatabaseHelper;
-import com.frostnerd.dnsserver.database.entities.main.DNSServerSetting;
 import com.frostnerd.dnsserver.database.entities.main.IPPortPair;
 import com.frostnerd.utils.networking.NetworkUtil;
 
-import java.util.HashMap;
 import java.util.regex.Pattern;
 
 /**
@@ -84,6 +84,29 @@ public final class Util {
             } else {
                 return null;
             }
+        }
+    }
+
+    public static String createNotificationChannel(Context context, boolean allowIconHiding){
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+            if(allowIconHiding){
+                NotificationChannel channel = new NotificationChannel("noIconChannel", context.getString(R.string.notification_channel_hiddenicon), NotificationManager.IMPORTANCE_MIN);
+                channel.enableLights(false);
+                channel.enableVibration(false);
+                channel.setDescription(context.getString(R.string.notification_channel_hiddenicon_description));
+                notificationManager.createNotificationChannel(channel);
+                return "noIconChannel";
+            }else{
+                NotificationChannel channel = new NotificationChannel("defaultchannel", context.getString(R.string.notification_channel_default), NotificationManager.IMPORTANCE_LOW);
+                channel.enableLights(false);
+                channel.enableVibration(false);
+                channel.setDescription(context.getString(R.string.notification_channel_default_description));
+                notificationManager.createNotificationChannel(channel);
+                return "defaultchannel";
+            }
+        }else{
+            return "defaultchannel";
         }
     }
 }
